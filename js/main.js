6e5eb47a517fb83187900e10b1620b2caa07d85b
zakirjabbar <mohdzakir200@gmail.com>
2022-09-19 22:01:22 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+    // $('.slick-slider').slick({
+    //   infinite: true,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1
+    // });
+
+    $('.slick-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
